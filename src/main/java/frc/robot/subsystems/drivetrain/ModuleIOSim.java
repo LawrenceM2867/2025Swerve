3ceb1f10c46bdb6b67b6668ca2cfe9e4aec1d042
JoshuaG-Y <hcps-groveryja@henricostudents.org>
2025-12-11 17:54:14 -0500

    Sim fixes sim fixes

--- src/main/java/frc/robot/subsystems/drivetrain/ModuleIOSim.java
+++ src/main/java/frc/robot/subsystems/drivetrain/ModuleIOSim.java
@@ -60,11 +60,13 @@ public class ModuleIOSim implements ModuleIO {
           driveFFVolts + driveController.calculate(driveSim.getAngularVelocityRadPerSec());
     } else {
       driveController.reset();
+      driveAppliedVolts = 0.0;
     }
     if (turnClosedLoop) {
       turnAppliedVolts = turnController.calculate(turnSim.getAngularPositionRad());
     } else {
       turnController.reset();
+      turnAppliedVolts = 0.0;
     }
 
     // Update sim state
@@ -112,6 +114,7 @@ public class ModuleIOSim implements ModuleIO {
   @Override
   public void setTPos(Rotation2d rotation) {
     turnClosedLoop = true;
+    turnController.reset();
     turnController.setSetpoint(rotation.getRadians());
   }
 }
